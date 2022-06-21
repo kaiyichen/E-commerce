d76593054c6f30c3626c0bf983f6d63bff9e49af
kaiyichen <chenkaiyi01@gmail.com>
2022-06-22 03:04:35 +0800

    v1.2

---
+++ products.js
@@ -0,0 +1,57 @@
+const products = [
+  {
+    id: 0,
+    name: "Wrap Dress",
+    price: 48,
+    instock: 100,
+    description:
+      "Calf-length dress in gently draping jersey with a V-neck and sewn-in wrapover with narrow ties at one side. Softly draping butterfly sleeves and a gently flared skirt.",
+    imgSrc: "images/ladies_clothes1",
+
+  },
+  {
+    id: 1,
+    name: "Ribbed Crew Neck Vest",
+    price: 29.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "images/ladies_clothes2",
+  },
+  {
+    id: 2,
+    name: "High-Waist Trousers",
+    price: 59.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "images/ladies_clothes3",
+  },
+  {
+    id: 3,
+    name: "Wrap Dress",
+    price: 78,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "images/ladies_clothes4",
+  },
+  {
+    id: 4,
+    name: "T-shirt 5",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "T-shirt 6",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t6.png",
+  },
+];
