61e410f3cc4870d5f204103fe35df8ffc6cd5ae2
kaiyichen <chenkaiyi01@gmail.com>
2022-06-22 08:45:07 +0800

    final version

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products = [
     instock: 100,
     description:
       "Calf-length dress in gently draping jersey with a V-neck and sewn-in wrapover with narrow ties at one side. Softly draping butterfly sleeves and a gently flared skirt.",
-    imgSrc: "images/ladies_clothes1.jpg",
+    imgSrc: "images/ladies_clothes1.1.jpg",
 
   },
   {
@@ -35,5 +35,186 @@ const products = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
     imgSrc: "images/ladies_clothes4.jpg",
+  },
+  {
+    id: 4,
+    name: "Cap",
+    price: 25,
+    instock: 5,
+    description:
+      "Cap",
+    imgSrc: "images/ladies_as1.jpg",
+  },
+  {
+    id: 5,
+    name: "Makeup Pouch",
+    price: 20,
+    instock: 100,
+    description:
+      "Cap",
+    imgSrc: "images/ladies_as2.jpg",
+  },
+  {
+    id: 6,
+    name: "Sling Bag",
+    price: 30,
+    instock: 100,
+    description:
+      "Bag",
+    imgSrc: "images/ladies_as3.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Scraf",
+    price: 15,
+    instock: 100,
+    description:
+      "Scarf",
+    imgSrc: "images/ladies_as4.jpg",
+  },
+  {
+    id: 8,
+    name: "Sandals",
+    price: 40,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/ladies_shoes1.jpg",
+  },
+  {
+    id: 9,
+    name: "Becs Mini Buckle Slide",
+    price: 24.99,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/ladies_shoes2.jpg",
+  },
+  {
+    id: 10,
+    name: "Harlow High Top",
+    price: 25,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/ladies_shoes3.jpg",
+  },
+  {
+    id: 11,
+    name: "Essential Tiana Slipper",
+    price: 12,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/ladies_shoes4.jpg",
+  },
+  {
+    id: 12,
+    name: "Button up shirt",
+    price: 30,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_clothes1.jpg",
+  },
+  {
+    id: 13,
+    name: "Singlet",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_clothes2.jpg",
+  },
+  {
+    id: 14,
+    name: "Jogger",
+    price: 59.90,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_clothes3.jpg",
+  },
+  {
+    id: 15,
+    name: "Short sleeves button up",
+    price: 78,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_clothes4.jpg",
+  },
+  {
+    id: 16,
+    name: "Cap",
+    price: 25,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_as1.jpg",
+  },
+  {
+    id: 17,
+    name: "Bucket Hat",
+    price: 20,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_as2.jpg",
+  },
+  {
+    id: 18,
+    name: "Bennie",
+    price: 30,
+    instock: 100,
+    description:
+      "Shirt",
+    imgSrc: "images/men_as3.jpg",
+  },
+  {
+    id: 19,
+    name: "Baseball Cap",
+    price: 15,
+    instock: 100,
+    description:
+      "Cap",
+    imgSrc: "images/men_as4.jpg",
+  },
+  {
+    id: 20,
+    name: "Sandals",
+    price: 40,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/men_shoes1.jpg",
+  },
+  {
+    id: 21,
+    name: "Sandals",
+    price: 24.99,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/men_shoes2.jpg",
+  },
+  {
+    id: 22,
+    name: "White trainers",
+    price: 25,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/men_shoes3.jpg",
+  },
+  {
+    id: 23,
+    name: "Black trainers",
+    price: 12,
+    instock: 100,
+    description:
+      "Shoe",
+    imgSrc: "images/men_shoes4.jpg",
   },
 ];
